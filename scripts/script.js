cb9cea4c83d85eebef8a2d8c3840d74e89f0a70f
sultonbayevogabek <sultonbayevogabek@gmail.com>
2021-04-04 01:32:54 +0500

    Make masked phone correct

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,13 @@ import siteNav from "./site-nav"
 import languageFunction from "./language"
 import scrollTop from "./scroll-top"
 import orderModal from "./order-modal"
+import maskedPhone from "./masked-phone"
 
 document.addEventListener('DOMContentLoaded', () => {
     searchInputFunction()
     siteNav()
     languageFunction()
     scrollTop()
+    maskedPhone()
     orderModal()
 })
